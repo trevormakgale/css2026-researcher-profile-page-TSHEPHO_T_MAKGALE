4260c60641df80adbc4e19f47d21c538884cf2e6
trevormakgale <140840463+trevormakgale@users.noreply.github.com>
2026-01-31 13:30:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,13 @@ import pandas as pd
 import numpy as np
 
 # Set page title
-st.set_page_config(page_title="Researcher Profile and STEM Data Explorer", layout="wide")
+st.set_page_config(
+    page_title="Researcher Profile and STEM Data Explorer",
+    page_icon="🔬",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
 
 # Sidebar Menu
 st.sidebar.title("Navigation")
